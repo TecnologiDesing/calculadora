911c52cff0ea3e34f7ff398321242cbdcbf54ff4
santiago velasquez <lsvelasquez06gmail.com>
2023-05-16 17:14:01 -0500

    develop 1.9

--- calculadora/calculadora/Program.cs
+++ calculadora/calculadora/Program.cs
@@ -45,171 +45,11 @@
 
 
             while (true)
-=======
+
             
         }
 
-        static void logica_de_Suma()
-        {
 
-
-            bool salir_a_menu = false;
-
-            while (!salir_a_menu)
->>>>>>> logica
-            {
-
-                try
-                {
-
-<<<<<<< HEAD
-                    int opciones;
-
-                    opciones = Convert.ToInt32(Console.ReadLine());
-
-                    switch (opciones)
-                    {
-
-                        case 1:
-                            Console.WriteLine();
-                            Console.WriteLine("       ╔══════════════════════════════╗");
-                            Console.WriteLine("       ║  Has elegido hacer una suma  ║");
-                            Console.WriteLine("       ╚══════════════════════════════╝");
-                            Console.WriteLine();
-                            logica_de_Suma();
-                            MostrarMenu();
-
-                            break;
-
-                        case 2:
-                            Console.WriteLine();
-                            Console.WriteLine("      ╔═══════════════════════════════╗");
-                            Console.WriteLine("      ║  Has elegido hacer una resta  ║");
-                            Console.WriteLine("      ╚═══════════════════════════════╝");
-                            Console.WriteLine();
-                            logica_de_resta();
-                            MostrarMenu();
-
-                            break;
-
-                        case 3:
-                            Console.WriteLine();
-                            Console.WriteLine("   ╔══════════════════════════════════╗");
-                            Console.WriteLine("   ║  Has elegido hacer una division  ║");
-                            Console.WriteLine("   ╚══════════════════════════════════╝");
-                            Console.WriteLine();
-                            logica_de_division();
-                            MostrarMenu();
-
-                            break;
-
-                        case 4:
-                            Console.WriteLine();
-                            Console.WriteLine("Has elegido hacer una multiplicacion");
-                            Console.WriteLine();
-                            logica_de_multiplicacion();
-                            MostrarMenu();
-
-                            break;
-
-                        case 5:
-                            Console.WriteLine();
-                            Console.WriteLine("   ╔══════════════════════════════════════╗");
-                            Console.WriteLine("   ║  Has elegido salir de la aplicación  ║");
-                            Console.WriteLine("   ╚══════════════════════════════════════╝");
-                            salir_de_app();
-
-                            break;
-
-
-
-                        default:
-                            Console.WriteLine();
-                            Console.WriteLine("╔════════════════════════════════════════════════╗");
-                            Console.WriteLine("║  Opcion incorrecta, ingrese una opcion valida  ║");
-                            Console.WriteLine("╚════════════════════════════════════════════════╝");
-                            Console.WriteLine();
-                            MostrarMenu();
-
-                            break;
-
-                    }
-=======
-                    // declaracion
-
-                    int numero1;
-                    int numero2;
-                    int resSuma;
-
-                    //perdir datos
-                    Console.WriteLine("        ╔════════════════════════════╗");
-                    Console.WriteLine("        ║  ingrese el primer numero  ║");
-                    Console.WriteLine("        ╚════════════════════════════╝");
-                    numero1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine();
-                    Console.WriteLine("        ╔════════════════════════════╗");
-                    Console.WriteLine("        ║  ingrese el segundo numero ║");
-                    Console.WriteLine("        ╚════════════════════════════╝");
-                    numero2 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine();
-                    salir_a_menu = true;
-                    // realizar operaciones
-
-                    resSuma = numero1 + numero2;
-
-                    // mostrar resultado
-                    Console.WriteLine("       ╔══════════════════════════════════════╗");
-                    Console.WriteLine("           El resultado de la suma es " + resSuma);
-                    Console.WriteLine("       ╚══════════════════════════════════════╝");
-                    Console.WriteLine();
->>>>>>> logica
-
-                }
-
-                catch (FormatException)
-
-                {
-
-                    Console.WriteLine();
-                    Console.WriteLine("       ╔═════════════════════════════╗");
-                    Console.WriteLine("       ║  usted no coloco un numero  ║");
-                    Console.WriteLine("       ╚═════════════════════════════╝");
-                    Console.WriteLine();
-<<<<<<< HEAD
-                    MostrarMenu();
-=======
->>>>>>> logica
-
-                }
-
-                catch (OverflowException)
-
-                {
-
-                    Console.WriteLine();
-                    Console.WriteLine("  ╔════════════════════════════════════════╗");
-                    Console.WriteLine("  ║  usted exedio el numero de caracteres  ║");
-                    Console.WriteLine("  ╚════════════════════════════════════════╝");
-                    Console.WriteLine();
-<<<<<<< HEAD
-                    MostrarMenu();
-=======
-
->>>>>>> logica
-                }
-
-            }
-
-<<<<<<< HEAD
-        }
-
-         //>>>>>>> opciones
-
-        
-
-=======
-
-        }
 
 
         static void logica_de_resta()
@@ -436,6 +276,7 @@
 
         }
 
->>>>>>> logica
+
+
     }
 }
