685536adddc8f999eac0c9ed92ca4a64a612b220
santiago velasquez <lsvelasquez06gmail.com>
2023-05-16 16:54:45 -0500

    se agg menu

--- calculadora/calculadora/Program.cs
+++ calculadora/calculadora/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+        }
+        static void MostrarMenu()
+        {
+
+            Console.WriteLine();
+            Console.WriteLine("    Elige la operacion que quiere realizar     ");
+            Console.WriteLine();
+            Console.WriteLine("╔════════════════════════════════════════════╗");
+            Console.WriteLine("║                * #1 Suma *                 ║");
+            Console.WriteLine("╚════════════════════════════════════════════╝");
+            Console.WriteLine("╔════════════════════════════════════════════╗");
+            Console.WriteLine("║                * #2 Resta *                ║");
+            Console.WriteLine("╚════════════════════════════════════════════╝");
+            Console.WriteLine("╔════════════════════════════════════════════╗");
+            Console.WriteLine("║               * #3 Dividir *               ║");
+            Console.WriteLine("╚════════════════════════════════════════════╝");
+            Console.WriteLine("╔════════════════════════════════════════════╗");
+            Console.WriteLine("║             * #4 Multiplicar *             ║");
+            Console.WriteLine("╚════════════════════════════════════════════╝");
+            Console.WriteLine("╔════════════════════════════════════════════╗");
+            Console.WriteLine("║                * #5 Salir *                ║");
+            Console.WriteLine("╚════════════════════════════════════════════╝");
+
         }
     }
 }
