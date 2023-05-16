e301e57694aafef2580978fc1518c41e9abd03f8
santiago velasquez <lsvelasquez06gmail.com>
2023-05-16 18:02:12 -0500

    develop 2.7

--- calculadora/calculadora/Program.cs
+++ calculadora/calculadora/Program.cs
@@ -6,7 +6,7 @@
 
         static void Main(string[] args)
         {
-<<<<<<< HEAD
+
 
          //<<<<<< HEAD
 
@@ -155,6 +155,76 @@
 
         }
 
+        static void logica_de_Suma()
+        {
+
+
+            bool salir_a_menu = false;
+
+            while (!salir_a_menu)
+            {
+
+                try
+                {
+
+                    // declaracion
+
+                    int numero1;
+                    int numero2;
+                    int resSuma;
+
+                    //perdir datos
+                    Console.WriteLine("        ╔════════════════════════════╗");
+                    Console.WriteLine("        ║  ingrese el primer numero  ║");
+                    Console.WriteLine("        ╚════════════════════════════╝");
+                    numero1 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    Console.WriteLine("        ╔════════════════════════════╗");
+                    Console.WriteLine("        ║  ingrese el segundo numero ║");
+                    Console.WriteLine("        ╚════════════════════════════╝");
+                    numero2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine();
+                    salir_a_menu = true;
+                    // realizar operaciones
+
+                    resSuma = numero1 + numero2;
+
+                    // mostrar resultado
+                    Console.WriteLine("       ╔══════════════════════════════════════╗");
+                    Console.WriteLine("           El resultado de la suma es " + resSuma);
+                    Console.WriteLine("       ╚══════════════════════════════════════╝");
+                    Console.WriteLine();
+
+                }
+
+                catch (FormatException)
+
+                {
+
+                    Console.WriteLine();
+                    Console.WriteLine("       ╔═════════════════════════════╗");
+                    Console.WriteLine("       ║  usted no coloco un numero  ║");
+                    Console.WriteLine("       ╚═════════════════════════════╝");
+                    Console.WriteLine();
+
+                }
+
+                catch (OverflowException)
+
+                {
+
+                    Console.WriteLine();
+                    Console.WriteLine("  ╔════════════════════════════════════════╗");
+                    Console.WriteLine("  ║  usted exedio el numero de caracteres  ║");
+                    Console.WriteLine("  ╚════════════════════════════════════════╝");
+                    Console.WriteLine();
+
+                }
+
+            }
+
+
+        }
 
         static void logica_de_resta()
         {
@@ -228,76 +298,7 @@
 
         }
 
-        static void logica_de_Suma()
-        {
-
-
-            bool salir_a_menu = false;
-
-            while (!salir_a_menu)
-            {
-
-                try
-                {
-
-                    // declaracion
-
-                    int numero1;
-                    int numero2;
-                    int resSuma;
-
-                    //perdir datos
-                    Console.WriteLine("        ╔════════════════════════════╗");
-                    Console.WriteLine("        ║  ingrese el primer numero  ║");
-                    Console.WriteLine("        ╚════════════════════════════╝");
-                    numero1 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine();
-                    Console.WriteLine("        ╔════════════════════════════╗");
-                    Console.WriteLine("        ║  ingrese el segundo numero ║");
-                    Console.WriteLine("        ╚════════════════════════════╝");
-                    numero2 = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine();
-                    salir_a_menu = true;
-                    // realizar operaciones
-
-                    resSuma = numero1 + numero2;
-
-                    // mostrar resultado
-                    Console.WriteLine("       ╔══════════════════════════════════════╗");
-                    Console.WriteLine("           El resultado de la suma es " + resSuma);
-                    Console.WriteLine("       ╚══════════════════════════════════════╝");
-                    Console.WriteLine();
-
-                }
-
-                catch (FormatException)
-
-                {
-
-                    Console.WriteLine();
-                    Console.WriteLine("       ╔═════════════════════════════╗");
-                    Console.WriteLine("       ║  usted no coloco un numero  ║");
-                    Console.WriteLine("       ╚═════════════════════════════╝");
-                    Console.WriteLine();
-
-                }
-
-                catch (OverflowException)
-
-                {
-
-                    Console.WriteLine();
-                    Console.WriteLine("  ╔════════════════════════════════════════╗");
-                    Console.WriteLine("  ║  usted exedio el numero de caracteres  ║");
-                    Console.WriteLine("  ╚════════════════════════════════════════╝");
-                    Console.WriteLine();
-
-                }
-
-            }
-
-
-        }
+      
 
 
         static void logica_de_division()
